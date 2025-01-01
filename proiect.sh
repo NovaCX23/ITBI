d7fd2888a630903ae681ba3a6399ca5333a04256
CxNova <xhamanichristian@gmail.com>
2025-01-01 20:56:04 +0200

    citire corecta

--- proiect.sh
+++ proiect.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+# Verificam daca s-a dat un director ca argument 
+if [ "$#" -lt 1 ]; then
+    echo "Utilizare: $0 [--follow-symlinks] <director>"
+    echo "Exemplu: $0 --follow-symlinks /etc"
+    exit 1
+fi
