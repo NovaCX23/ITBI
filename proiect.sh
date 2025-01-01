061276480a85dc076827fc9ddcc9e93bbd3ce7c2
CxNova <xhamanichristian@gmail.com>
2025-01-01 21:06:34 +0200

    verificare argumente

--- proiect.sh
+++ proiect.sh
@@ -6,3 +6,15 @@ if [ "$#" -lt 1 ]; then
     echo "Exemplu: $0 --follow-symlinks /etc"
     exit 1
 fi
+
+# Initializare varibile
+follow_symlinks=0
+start_dir=""
+
+# Verif primul argument 
+if [ "$1" == "--follow-symlinks" ]; then
+    follow_symlinks=1
+    start_dir="2"         # Directorul este al doilea arg
+else
+    start_dir="1"         # Dir este primul arg
+fi
